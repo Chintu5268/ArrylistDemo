004a5f91007f0a446497c79c26fe7f5df45feaab
Sandesh Sarade <Saradesandesh@gmail.com>
2024-01-29 18:49:13 +0530

    added arraylist class

---
+++ src/com/arrylist/ArrayProject.java
@@ -0,0 +1,15 @@
+package com.arrylist;
+
+import java.util.ArrayList;
+
+public class ArrayProject {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList<>();
+        al.add("Pune");
+        al.add("Nagpur");
+        al.add("Nashik");
+        al.add("Mumbai");
+        al.add("Delhi");
+        System.out.println(al);
+    }
+}
